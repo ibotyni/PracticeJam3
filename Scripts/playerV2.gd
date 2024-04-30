36b98d1b14db2152a6a2cd14783e229dc85003ba
total1ty2-sudo <154597951+total1ty2-sudo@users.noreply.github.com>
2024-04-30 20:26:40 +0200

    basic animation part2

--- Scripts/playerV2.gd
+++ Scripts/playerV2.gd
@@ -1,39 +1,51 @@
 extends CharacterBody2D
- 
+
 var direction : Vector2 = Vector2.ZERO
 var swing : bool = false
- 
-@onready var animation_tree = $AnimationTree
- 
+
+var last_direction = "Down"
+
+
 func _physics_process(_delta):
 	if not swing:
 		velocity = direction * 75
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
- 
+
 func _process(_delta):
 	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
- 
+	
 	if direction != Vector2.ZERO and not swing:
-		set_walking(true)
-		update_blend_position()
+		set_walking(direction)
+		#update_blend_position()
 	else:
-		set_walking(false)
- 
+		set_walking(Vector2.ZERO)
+	
 	if Input.is_action_just_pressed("swing"):
 		set_swing(true)
- 
+
 func set_swing(value = false):
 	swing = value
-	animation_tree["parameters/conditions/swing"] = value
- 
+	#animation_tree["parameters/conditions/swing"] = value
+
 func set_walking(value):
+	if value[1] == -1: # 0,-1
+		last_direction = "Up"
+	if value[1] == 1: # 0,1
+		last_direction = "Down"
+	if value[0] == -1: # -1,0
+		last_direction = "Left"
+	if value[0] == 1: # 1,0
+		last_direction = "Right"
+
+	if value == Vector2.ZERO:
+		$AnimatedSprite2D.play("Idle_" + last_direction)
+	else:
+		$AnimatedSprite2D.play("Walk_" + last_direction)
 	
-	animation_tree["parameters/conditions/is_walking"] = value
-	animation_tree["parameters/conditions/idle"] = not value
- 
-func update_blend_position():
-	animation_tree["parameters/attack/blend_position"] = direction
-	animation_tree["parameters/idle/blend_position"] = direction
-	animation_tree["parameters/walk/blend_position"] = direction
+
+#func update_blend_position():
+	#animation_tree["parameters/attack/blend_position"] = direction
+	#animation_tree["parameters/idle/blend_position"] = direction
+	#animation_tree["parameters/walk/blend_position"] = direction
