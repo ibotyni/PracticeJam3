107fda9cb62c52456a38dc91c321c090e9e5cbc6
Jacmac <jacmac98@Live.com>
2024-05-01 13:58:25 +1000

    Updates to MenuWork

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -1,7 +1,21 @@
-extends Control
+extends CanvasLayer
+@onready var pause_menu = $"."
 
 #Pause Function, watches for PAUSE key press, then pauses game, opens pause menu.
-#func watchForPause():
-#	if Input.is_action_pressed("Pause"):
-#		print("Paused?")
-#		get_tree().paused = true
+#Watching for Esc Press every frame
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		print(event.as_text())
+		pause_menu.visible = true
+		get_tree().paused = true
+
+#On Resume Button
+func _on_pause_resume_pressed():
+	pause_menu.visible = false
+	get_tree().paused = false
+
+#Back to Main Menu
+func _on_pause_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
+	
