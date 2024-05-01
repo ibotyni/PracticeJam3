df72841936f234e30ab41707f235bfc628e8a040
total1ty2-sudo <154597951+total1ty2-sudo@users.noreply.github.com>
2024-05-01 18:34:48 +0200

    15 levels done

--- Scripts/playerV2.gd
+++ Scripts/playerV2.gd
@@ -6,10 +6,8 @@ var swing : bool = false
 var last_direction = "Down"
 @export var Bullet : PackedScene
 
-var direction : Vector2 = Vector2.ZERO
-var swing : bool = false
 
-var last_direction = "Down"
+
 
 
 func _physics_process(_delta):
@@ -17,7 +15,6 @@ func _physics_process(_delta):
 		velocity = direction * 75
 	else:
 		velocity = Vector2.ZERO
-func _physics_process(_delta):
 	if not swing:
 		velocity = direction * 75
 	else:
@@ -33,12 +30,8 @@ func _process(_delta):
 	else:
 		set_walking(Vector2.ZERO)
 	
-	if Input.is_action_just_pressed("swing"):
-		set_swing(true)
 
-func set_swing(value = false):
-	swing = value
-	#animation_tree["parameters/conditions/swing"] = value
+
 
 func set_walking(value):
 	if value[1] == -1: # 0,-1
@@ -61,39 +54,4 @@ func set_walking(value):
 	#animation_tree["parameters/idle/blend_position"] = direction
 	#animation_tree["parameters/walk/blend_position"] = direction
 
-func _process(_delta):
-	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	
-	if direction != Vector2.ZERO and not swing:
-		set_walking(direction)
-		#update_blend_position()
-	else:
-		set_walking(Vector2.ZERO)
-	
-	if Input.is_action_just_pressed("swing"):
-		set_swing(true)
 
-func set_swing(value = false):
-	swing = value
-	#animation_tree["parameters/conditions/swing"] = value
-
-func set_walking(value):
-	if value[1] == -1: # 0,-1
-		last_direction = "Up"
-	if value[1] == 1: # 0,1
-		last_direction = "Down"
-	if value[0] == -1: # -1,0
-		last_direction = "Left"
-	if value[0] == 1: # 1,0
-		last_direction = "Right"
-
-	if value == Vector2.ZERO:
-		$AnimatedSprite2D.play("Idle_" + last_direction)
-	else:
-		$AnimatedSprite2D.play("Walk_" + last_direction)
-	
-
-#func update_blend_position():
-	#animation_tree["parameters/attack/blend_position"] = direction
-	#animation_tree["parameters/idle/blend_position"] = direction
-	#animation_tree["parameters/walk/blend_position"] = direction
