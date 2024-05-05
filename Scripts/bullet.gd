238172b539222480afb1d9d12acf7eac09bd6df8
fozzedout <fozzedout@gmail.com>
2024-05-05 03:07:23 +0100

    added spawning enemies & removed old levels
    removed old levels 1-15
    duplicated level 0 as level 1 but without pre spawned enemies

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -8,7 +8,6 @@ func _physics_process(delta):
 	position += transform.x * speed * delta
 
 func _on_body_entered(body):
-	print(body.name)
 	if body.is_in_group("enemy"):
 		body.take_damage(damage)
 	queue_free()
