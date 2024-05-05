a8878ada44195738979998d6fe6ede8912dcb5b6
samratbs <samratbs76@gmail.com>
2024-05-05 23:46:11 +0200

    hazard updates

--- Scripts/Hazards/cracked_floor.gd
+++ Scripts/Hazards/cracked_floor.gd
@@ -10,16 +10,15 @@ var collapsed := false
 func _process(delta):
 	if !collapsed:
 		var touching = get_overlapping_areas()
-		if touching.is_empty() && !collapse_timer.is_stopped():
-			is_collapsing = false
-			print("stopped collapsing")
-			collapse_timer.stop()
-		elif !touching.is_empty() && collapse_timer.is_stopped():
+		#if touching.is_empty() && !collapse_timer.is_stopped():
+			#is_collapsing = false
+			#print("stopped collapsing")
+			#collapse_timer.stop()
+		if !touching.is_empty() && collapse_timer.is_stopped():
 			for area in touching:
 				var entity = area.get_parent()
 				if entity is Player || entity is Enemy:
 					is_collapsing = true
-					print("collapsing")
 					collapse_timer.start()
 
 func collapse() -> void:
